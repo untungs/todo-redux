d53c62e7bb9420fe1fe313b0b946572b509012b0
Untung Suryono <untungs30@gmail.com>
2017-09-29 17:08:56 +0700

    add redux store, actions, and reducers

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux';
+import todoApp from './src/redux/reducers'
 import Root from './src/Root'
 
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.store = createStore(todoApp)
+  }
+
   render() {
     return (
-      <Root />
+      <Provider store={this.store}>
+        <Root />
+      </Provider>
     )
   }
 }
