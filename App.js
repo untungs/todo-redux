52c1b1f2b846b89fbaa2bef54b634d005af924d7
Untung Suryono <untungs30@gmail.com>
2017-09-30 15:58:25 +0700

    save state with redux-persist
    - yarn add redux-persist

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import { createStore, applyMiddleware } from 'redux'
+import { AsyncStorage } from 'react-native';
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux';
+import { persistStore, autoRehydrate } from 'redux-persist'
 import { createLogger } from 'redux-logger';
 import todoApp from './src/redux/reducers'
 import Root from './src/Root'
@@ -8,8 +10,12 @@ import Root from './src/Root'
 export default class App extends React.Component {
   constructor() {
     super()
-    const logger = createLogger({ collapsed: true, duration: true })
-    this.store = createStore(todoApp, applyMiddleware(logger))
+    const logger = createLogger({ duration: true })
+    this.store = createStore(
+      todoApp, 
+      compose(applyMiddleware(logger), autoRehydrate())
+    )
+    persistStore(this.store, { storage: AsyncStorage })
   }
 
   render() {
